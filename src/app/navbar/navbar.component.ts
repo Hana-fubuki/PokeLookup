0003ab52a5f78b1f072a297bc5d696602554f32a
Hana-fubuki <jgcoheno@gmail.com>
2020-09-27 00:34:41 -0400

    updating configs, and changed nav and service to work better

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,62 +10,34 @@ declare var require: any;
 export class NavbarComponent implements OnInit {
   generationsList: any[] = [];
   pokemonList: any[] = [];
+
   constructor(private pss: PokemonServiceService) { }
 
   ngOnInit() {
     this.getGenerations();
   }
 
-  getRegion(genID: number) {
+  getGenerationByRegion(genID: number) {
     console.log('genID vale: ', genID);
     let limit = 10000;
     let offset = 0;
     // @ts-ignore
-    switch (genID) {
-      case -1: {
-        break;
-      }
-      case 0: {
-        limit = 151;
-        break;
-      }
-      case 1: {
-        limit = 99;
-        offset = 152;
-        break;
-      }
-      case 2: {
-        offset = 252;
-        limit = 134;
-        break;
-      }
-      case 3: {
-        offset = 387;
-        limit = 106;
-        break;
-      }
-      case 4: {
-        offset = 494;
-        limit = 155;
-        break;
-      }
-      case 5: {
-        offset = 650;
-        limit = 71;
-        break;
-      }
-      case 6: {
-        offset = 722;
-        limit = 87;
-        break;
-      }
+    if(genID === -1){
+      this.pss.getAll(offset, limit).subscribe( (res: Response) => {
+        // @ts-ignore
+        this.pokemonList = res.results;
+        // @ts-ignore
+        console.log('pokemon :', res);
+      });
     }
-    this.pss.getRegion(offset, limit).subscribe( (res: Response) => {
-      // @ts-ignore
-      this.pokemonList = res.results;
-      // @ts-ignore
-      console.log('pokemon :', res);
-    });
+    else {
+      this.pss.getGeneration(genID).subscribe((res: Response) => {
+        // @ts-ignore
+        this.pokemonList = res.results;
+        // @ts-ignore
+        console.log('pokemon :', res);
+      });
+    }
   }
 
   getGenerations() {
@@ -73,7 +45,7 @@ export class NavbarComponent implements OnInit {
       // @ts-ignore
       this.generationsList = res.results;
       // @ts-ignore
-      console.log('Generations :', res.results);
+      console.log('pokemon :', res);
     });
   }
 }
