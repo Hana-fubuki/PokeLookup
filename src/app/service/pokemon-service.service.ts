fab443f7ac5df8cdfd8e8822a1d6ce279ae95e19
Hana-fubuki <jgcoheno@gmail.com>
2020-09-26 22:42:54 -0400

    updating packages

--- src/app/service/pokemon-service.service.ts
+++ src/app/service/pokemon-service.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Pokedex } from "pokedex-promise-v2";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonServiceService {
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, public pokedex: Pokedex) {
+    this.pokedex = new Pokedex();
+  }
 
-  getRegion(offset: number, limit: number) {
-    console.log('in getRegion service with offset', offset, ' and limit ', limit);
-    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`);
+  getRegion() {
+    return this.pokedex.getGenerationsList().then(function(response) {
+      console.log(response);
+    })
   }
 
   getGenerations(): Observable<any> {
